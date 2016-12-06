a0d1d757ed5bdae0a93747b770dcde82383323ee
Edgar Honing <ephoning@gmail.com>
2016-12-05 21:31:51 -0700

    remove obsolete

--- src/clp_intro/append_clp.clj
+++
@@ -1,6 +0,0 @@
-(ns clp-intro.append-clp
-  (:require [clojure.core.logic :refer :all]))
-
-;; unify lvar with itself (i.e., "promote" keyword to lvar) or passed-in value
-(defn to-lvar [a aL]
-  (== aL (if (keyword? a) aL a)))
--- src/clp_intro/append_functional.clj
+++
@@ -1,7 +0,0 @@
-(ns clp-intro.append-functional)
-
-(defn append
-  " append 2 seqs"
-  [a b]
-  (letfn [(_append [a b] (if (empty? a) b (_append (rest a) (cons (first a) b))))]
-    (_append (reverse a) b)))
