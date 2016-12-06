d9a2b604ffa53f1aa39ef1ca77d65741a0e3a4f5
Edgar Honing <ephoning@gmail.com>
2016-12-05 21:31:18 -0700

    add some

---
+++ src/clp_intro/append.clj
@@ -0,0 +1,18 @@
+(ns clp-intro.append
+  (:require [clojure.core.logic :refer :all]))
+
+(defn append
+  "append 2 seqs - functional"
+  [a b]
+  (if (empty? a) b (cons (first a)(append (rest a) b))))
+
+(defn appendo
+  "append 2 seqs - relational"
+  [l s out]
+  (conde
+   [(== '() l) (== s out)]
+   [(fresh [a d r]
+      (== (lcons a d) l)
+      (== (lcons a r) out)
+      (appendo d s r))])) ; make recursive call last to avoid non-termination
+                          ; when asking for non-existent answers
---
+++ src/clp_intro/member.clj
@@ -0,0 +1,19 @@
+(ns clp-intro.member
+  (:require [clojure.core.logic :refer :all]))
+
+(defn member
+  "find in seq + return remainder - functional"
+  [x l]
+  (cond
+    (empty? l) false
+    (first x l) l
+    else (member x (rest l))))
+
+(defn membero
+  "find in seq + return remainder - relational"
+  [x l out]
+  (conde
+   [(== '() l) (== false out)]
+   [(== )]
+   [])
+)
