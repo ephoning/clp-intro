09a8c4c8ea922283c119f9f78e8f38992773c143
Edgar Honing <ehoning@overstock.com>
2016-12-12 11:07:31 -0700

    fix doc strings + add more read-me info

--- src/clp_intro/peano.clj
+++ src/clp_intro/peano.clj
@@ -3,23 +3,22 @@
             [clojure.string :as str]
             :reload))
 
-(defn any-val? [x]
-  (str/starts-with? "_" (str x)))
 
 ;; convert natural number to Peano representation
 ;; (if given a keyword, leave it untouched)
-(defn n->peano [n]
+(defn n->peano
   "convert natural number to peano representation (but leave keywords as-is)"
+  [n]
   (if (keyword? n) n (if (= n 0) () (cons \S (n->peano (dec n))))))
 
 (defn peano? [p]
   (and (seq? p) (or (empty? p) (= (first p) \S))))
 
-(defn peano->n [ps]
+(defn peano->n
   "convert potentially nested peano representation(s) to natural number(s)"
+  [ps]
   (cond (peano? ps) (count ps)
-        (symbol? ps) :any
-;;        (any-val? ps) :any
+        (symbol? ps) ps ; ('_0' or '_1', etc.)
         :else (cons (peano->n (first ps)) (if (empty? (rest ps)) () (peano->n (rest ps))))))
 
 ;; examples:
@@ -47,32 +46,41 @@
 ;; --------------------------------------
 ;; (relational helpers)
 ;;
-(defn to-lvar [v vL]
+(defn to-lvar
   "unify lvar with itself (no-op) or passed-in value
   (this, in effect, 'promotes' a keyword to actual lvar)"
+  [v vL]
   (== vL (if (keyword? v) vL v)))
 
-(defn call-run* [r a b c]
-  "run* wrapper"
-  (run* [q]
-    (fresh [a-l b-l c-l]
-      (to-lvar a a-l)
-      (to-lvar b b-l)
-      (to-lvar c c-l)
-      (r a-l b-l c-l)
-      (== q (list a-l b-l c-l)))))
+(defn call-relation [r a b c q]
+  (fresh [a-l b-l c-l]
+    (to-lvar a a-l)
+    (to-lvar b b-l)
+    (to-lvar c c-l)
+    (r a-l b-l c-l)
+    (== q (list a-l b-l c-l))))
+
+(defn call-run
+  "run* / run n wrapper"
+  ([r a b c]
+   (run* [q] (call-relation r a b c q)))
+  ([r n a b c]
+   (run n [q] (call-relation r a b c q))))
+
 ;; --------------------------------------
 ;; (relational)
 
-(defn add-peano-r [x y s]
-  "sum of 2 peano numbers"
-  (call-run* appendo x y s))
+(defn add-peano-r
+  "x + y = s"
+  [x y s]
+  (call-run appendo x y s))
 
 (defn add-r [x y s]
   (peano->n (add-peano-r (n->peano x) (n->peano y) (n->peano s))))
 
-(defn sub-peano-r [x y d]
-  "difference between 2 peano numbers"
+(defn sub-peano-r
+  "x - y = d"
+  [x y d]
   (let [r (add-peano-r d y x)]
     (loop [l r accu ()]
       (if (empty? l) accu
@@ -82,8 +90,10 @@
   (peano->n (sub-peano-r (n->peano x) (n->peano y) (n->peano d))))
 
 
-(defn multo-v1 [x y p]      ; x * y = p
-  "broken: does not terminate for the following argument sets: (:x () (\S ...)) (:x :y (\S ...))"
+(defn multo-v1
+  "x * y = p
+   broken: does not terminate for the following argument sets: (:x () (S ...)) (:x :y (S ...))"
+  [x y p]
   (conde
    [(== () x) (== () p)]
    [(fresh [a b c]
@@ -91,11 +101,13 @@
       (appendo y c p)       ; y + c = p   ==   y + (x - 1) * y = p
       (multo-v1 b y c))]))  ; (x - 1) * y = c
 
-(defn multo-v2 [x y p]      ; x * y = p
-  "semi-broken: terminate for the following argument sets: (:x () (\S ...)) (:x :y (\S ...))
+(defn multo-v2
+  "x * y = p
+   semi-broken; now terminats for the following argument sets: (:x () (S ...)) (:x :y (S ...))
    but results look like:
-   (mult-r :x :y 3) ; (((_0 _1 _2) (\S) (\S \S \S)) ((_0) (\S \S \S) (\S \S \S)))
+   (mult-r :x :y 3) ; (((_0 _1 _2) (S) (S S S)) ((_0) (S S S) (S S S)))
    reason: head elements (i.e., lvars a & d below) are/remain lvars"
+  [x y p]
   (conde
    [(== () x) (== () p)]
    [(== () y) (== () p)]
@@ -105,39 +117,58 @@
       (appendo y c p)       ; y + c = p   ==   y + (x - 1) * y = p
       (multo-v2 xt y c))])) ; (x - 1) * y = c
 
-(defn multo-v3 [x y p]      ; x * y = p
-  "fix for introduction of lvars as head elements
+(defn multo-v3
+  "x * y = p
+   fix for introduction of lvars as head elements: unify head elements with S
    but results can look like:
   (peano->n (mult-r 0 :y 0)) ; ((0 :any 0) (0 0 0))
   reason: TODO"
+  [x y p]
   (conde
    [(== () x) (== () p)]
    [(== () y) (== () p)]
    [(fresh [xt yt c]
-      (== (lcons \S xt) x)  ; (if x is an lvar, head is unified with a proper \S)
-      (== (lcons \S yt) y)  ; (if y is an lvar, head is unified with a proper \S)
+      (== (lcons \S xt) x)
+      (== (lcons \S yt) y)
       (appendo y c p)
       (multo-v3 xt y c))]))
 
-(defn mult-peano-r [x y p]
+(defn multo-v4
+  "x * y = p"
+  [x y p]
+  (conde
+   [(== () x) (== () p)]
+   [(== () y) (== () p)]
+   [(fresh [xt yt c]
+      (== (lcons \S xt) x)
+      (== (lcons \S yt) y)
+      (appendo y c p)
+      (multo-v4 xt y c))]))
+
+(def multo multo-v4)
+
+(defn mult-peano-r
   "product of 2 peano numbers"
-  (call-run* multo-v3 x y p))
+  ([x y p] (call-run multo x y p))
+  ([x y p n] (call-run multo n x y p)))
 
-(defn mult-r [x y p]
-  ;;  (peano->n (mult-peano-r (n->peano x) (n->peano y) (n->peano p)))
-  (mult-peano-r (n->peano x) (n->peano y) (n->peano p))
-  )
+(defn mult-r
+  "x * y = p"
+  ([x y p] (peano->n (mult-peano-r (n->peano x) (n->peano y) (n->peano p))))
+  ([x y p n] (peano->n (mult-peano-r (n->peano x) (n->peano y) (n->peano p) n))))
 
 
-(defn div-peano-r [x y q]
-  "difference between 2 peano numbers"
+(defn div-peano-r
+  "x / y = q"
+  [x y q]
   (let [r (mult-peano-r q y x)]
     (loop [l r accu ()]
       (if (empty? l) accu
           (recur (rest l) (cons (let [[q y x] (first l)] `(~x ~y ~q)) accu))))))
 
-(defn div-r [x y q]
-  "quotient of 2 peano numbers"
+(defn div-r
+  "x / y = q"
+  [x y q]
   (peano->n (div-peano-r (n->peano x) (n->peano y) (n->peano q)))  )
 
 
